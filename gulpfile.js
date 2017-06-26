55489f41f46ea238ae19b4a30f8d5b77ab0116f3
Damien Asseya <dasseya1@gmail.com>
2017-06-26 16:21:16 +0000

    Moved files to public folder and added express

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,40 @@ var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
+var nodemon = require('gulp-nodemon');
+
+'use strict';
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var nodemon = require('gulp-nodemon');
+
+
+gulp.task('default', ['browser-sync'], function () {
+});
+
+gulp.task('browser-sync', ['nodemon'], function() {
+	browserSync.init(null, {
+		proxy: "http://localhost:5000",
+        files: ["public/**/*.*"],
+        browser: "google chrome",
+        port: 7000,
+	});
+});
+gulp.task('nodemon', function (cb) {
+	
+	var started = false;
+	
+	return nodemon({
+		script: 'app.js'
+	}).on('start', function () {
+		if (!started) {
+			cb();
+			started = true; 
+		} 
+	});
+});
 
 gulp.task('scripts', function() {
     return gulp.src('js/scripts.js')

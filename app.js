32f9c45cb376352eec1e1cd0e1da5477472f6cdf
Damien Asseya <etudescanada01@gmail.com>
2017-09-22 15:57:05 -0400

    Changed PORT

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get('/', function(req, res) {
     res.sendfile('./public/index.html');
 });
 
-app.listen(process.env.PORT || 3000 ,function(){
+app.listen(process.env.PORT || 8080 ,function(){
     console.log("up and running on port "+ process.env.PORT);
 });
